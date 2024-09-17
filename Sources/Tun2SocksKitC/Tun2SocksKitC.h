4eb93884b782f0d1b96d2be7e57b24409f69f31c
Alexander Pashinin <alxpashinin@gmail.com>
2024-09-17 18:41:22 +0500

    Update Tun2SocksKitC.h
    Add support to xcode 16

--- Sources/Tun2SocksKitC/Tun2SocksKitC.h
+++ Sources/Tun2SocksKitC/Tun2SocksKitC.h
@@ -5,6 +5,12 @@
 
 #define CTLIOCGINFO 0xc0644e03UL
 
+typedef uint8_t  u_int8_t;
+typedef uint16_t u_int16_t;
+typedef uint32_t u_int32_t;
+typedef uint64_t u_int64_t;
+typedef unsigned char u_char;
+
 struct ctl_info {
     u_int32_t   ctl_id;
     char        ctl_name[96];
